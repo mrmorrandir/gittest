e3376e99c3fe03f28d2b743067f4df8db29116f6
mrmorrandir <newsgroups@morrandir.de>
2022-12-20 09:45:43 +0100

    Feature MagicService 3

---
+++ src/GitTest/MagicService3.cs
@@ -0,0 +1,13 @@
+﻿using GitTest.Abstractions;
+
+namespace GitTest;
+
+public class MagicService3 : IMagicService
+{
+    public string GetMagicString()
+    {
+        var number = new Random().Next();
+        // Comment for the magic string
+        return $"A number #{number} is magic!";
+    }
+}
---
+++ tests/GitTest.UnitTests/MagicService3Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace GitTest.UnitTests;
+
+public class MagicService3Tests
+{
+    [Fact]
+    public void ShouldReturnMagicString_WhenCalled()
+    {
+        var magicService = new MagicService3();
+        
+        var result = magicService.GetMagicString();
+        
+        result.Should().Match("*Magic*number*");
+    }
+}
